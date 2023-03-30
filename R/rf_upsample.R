cbe68e783340303e741eb24b1c13882a7c3bdc9a
Jake Rhodes <jakerhodes@isu.edu>
2023-03-30 15:56:42 -0600

    added template to generate function

--- R/rf_upsample.R
+++ R/rf_upsample.R
@@ -37,3 +37,72 @@ rf_upsample <- function(x, y, rf = NULL, type = 'rfgap', x_test = NULL,
 
 
 }
+
+################################################################################
+# Iris Example
+################################################################################
+# x <- iris[, 1:4]
+# y <- iris[, 5]
+
+# rf <- ranger(x = x, y = y, write.forest = TRUE, keep.inbag = TRUE)
+# prox <- get_proximities(x, rf = rf, type = 'rfgap')
+
+
+# samp <- sample(1:150, 10, replace = TRUE)
+# ext <- rowSums(t(x[samp, ]) %*% prox[samp, ]) / length(samp)
+
+
+
+# nobs <- 1000
+# nsamps <- 3
+# exts <- matrix(rep(0, 4 * 3 * nobs), ncol = 4)
+# # exts <- matrix(rep(0, 4 * nobs), ncol = 4)
+# # plot(x[, 1], x[, 3])
+
+# for (i in 1:nobs) {
+#   samp <- sample(which(y == 'setosa'), nsamps, replace = TRUE)
+#   ext <- rowSums(t(x[samp, ]) %*% prox[samp, ]) / length(samp)
+#   exts[i, ] <- ext
+# }
+
+# for (i in 1:nobs) {
+#   samp <- sample(which(y == 'virginica'), nsamps, replace = TRUE)
+#   ext <- rowSums(t(x[samp, ]) %*% prox[samp, ]) / length(samp)
+#   exts[i + nobs, ] <- ext
+# }
+
+# for (i in 1:nobs) {
+#   samp <- sample(which(y == 'versicolor'), nsamps, replace = TRUE)
+#   ext <- rowSums(t(x[samp, ]) %*% prox[samp, ]) / length(samp)
+#   exts[i + 2 * nobs, ] <- ext
+# }
+
+# exts.df <- data.frame('Sepal.Length' = jitter(exts[, 1]),
+#                       'Sepal.Width'  = jitter(exts[, 2]),
+#                       'Petal.Length' = jitter(exts[, 3]),
+#                       'Petal.Width'  = jitter(exts[, 4]))
+
+# plot(exts.df)
+
+# pairs(x)
+
+
+
+#################################################################################
+# MNIST EXAMPLE
+#################################################################################
+
+# data <- as.data.frame(fread('datasets/mnist_test.csv'))
+#
+#
+# x <- data[, -1]
+# y <- data[, 1]
+#
+# rf <- ranger(x = x, y = y, write.forest = TRUE, keep.inbag = TRUE)
+# prox <- get_proximities(x, rf = rf, type = 'rfgap')
+#
+#
+# samp <- sample(which(y == 7), 3, replace = TRUE)
+# ext <- rowSums(t(x[samp, ]) %*% prox[samp, ]) / length(samp)
+# ext_im <- matrix(ext, nrow = 28, byrow = TRUE)
+# imagesc(ext_im)
